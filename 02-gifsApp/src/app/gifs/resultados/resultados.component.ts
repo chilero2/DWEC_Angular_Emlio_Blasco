5ed36107dc1d170715913b97c942d0ed7269eafc
emilio <emilioblasco@icloud.com>
2023-01-27 09:04:27 +0100

    tipado de los gif

--- 02-gifsApp/src/app/gifs/resultados/resultados.component.ts
+++ 02-gifsApp/src/app/gifs/resultados/resultados.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../service/gifs.service';
 
 @Component({
   selector: 'app-resultados',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./resultados.component.css']
 })
 export class ResultadosComponent {
+
+  get resultados() {
+    return this.gifsService.resultados
+  }
+
+  constructor(private gifsService:GifsService){}
 
 }
--- 02-gifsApp/src/app/gifs/service/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/service/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Gif, SearchGIFResponse } from '../interfaces/gif.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ export class GifsService {
 
   private apiKey: string = '0ZuFrrnLjnMZDmyr0YCOlHvYR1j8J2LO'
   private _historial: string[] = []
+  public resultados :Gif[] = []
 
   get historial() {
     return [...this._historial]
@@ -17,13 +19,16 @@ export class GifsService {
   buscarGifs(query:string){
     if (query.trim().length === 0) return
     if (this._historial.includes(query.toLowerCase())) return
+
     
     this._historial.unshift(query.toLowerCase())
     this._historial = this._historial.splice(0,10)
 
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=0ZuFrrnLjnMZDmyr0YCOlHvYR1j8J2LO&q=dragon&limit=10')
-    .subscribe((res:any) => {
+    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=0ZuFrrnLjnMZDmyr0YCOlHvYR1j8J2LO&q=${query}&limit=10`)
+    .subscribe((res) => {
       console.log(res.data)
+      console.log(query)
+      this.resultados = res.data
     })
 
   }
